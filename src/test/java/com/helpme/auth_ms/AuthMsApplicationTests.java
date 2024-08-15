9f4b6523899a807d53867eb0dd9e37ea65cf8b7f
Robson Fischer <fischerrobson@gmail.com>
2024-08-15 11:46:09 -0300

    solved contextLoads test error

--- src/test/java/com/helpme/auth_ms/AuthMsApplicationTests.java
+++ src/test/java/com/helpme/auth_ms/AuthMsApplicationTests.java
@@ -1,10 +1,25 @@
 package com.helpme.auth_ms;
 
+import com.helpme.auth_ms.repositories.UserRepository;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class AuthMsApplicationTests {
+
+	@BeforeAll
+	static void setUp() {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+		System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+		System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+	}
+
 
 	@Test
 	void contextLoads() {
