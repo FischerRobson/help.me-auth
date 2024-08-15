44a124560a7d6aafbd4de2e0276d1fdc4b1735dc
Robson Fischer <fischerrobson@gmail.com>
2024-08-15 12:52:09 -0300

    fixed env loads from .env only when it exists

--- src/main/java/com/helpme/auth_ms/AuthMsApplication.java
+++ src/main/java/com/helpme/auth_ms/AuthMsApplication.java
@@ -8,11 +8,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthMsApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
-		System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
-		System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
-		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		// Check if running locally with a .env file
+		Dotenv dotenv = null;
+		try {
+			dotenv = Dotenv.load();
+		} catch (Exception e) {
+			System.out.println(".env file not found, assuming environment variables are set externally.");
+		}
+
+		// If .env is loaded, use it; otherwise, rely on system environment variables
+		if (dotenv != null) {
+			System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+			System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+			System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
+			System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		}
+
 
 		SpringApplication.run(AuthMsApplication.class, args);
 	}
