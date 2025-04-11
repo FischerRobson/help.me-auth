fee87ede8edd8fdbf6068f34e34b5b6d48ec9527
Robson Fischer <fischerrobson@gmail.com>
2025-04-10 22:00:27 -0300

    add: github action to validate pull requests

--- src/test/java/com/helpme/auth_ms/services/AuthServiceTest.java
+++ src/test/java/com/helpme/auth_ms/services/AuthServiceTest.java
@@ -103,4 +103,9 @@ class AuthServiceTest {
         verify(userRepository).findByEmail(login.getEmail());
         verify(passwordEncoder).matches(login.getPassword(), testUser.getPassword());
     }
+
+    @Test
+    void failTestToCheckGithubAction() {
+        assertEquals(2, 3);
+    }
 }
