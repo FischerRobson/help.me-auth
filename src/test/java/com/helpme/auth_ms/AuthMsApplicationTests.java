c4d047403a01492f615e43928ca86ecc487ca15d
Robson Fischer <fischerrobson@gmail.com>
2025-04-10 22:17:37 -0300

    fix: contextLoads test

--- src/test/java/com/helpme/auth_ms/AuthMsApplicationTests.java
+++ src/test/java/com/helpme/auth_ms/AuthMsApplicationTests.java
@@ -8,7 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb", // in-memory DB
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+		"spring.jpa.hibernate.ddl-auto=none"
+})
+@ActiveProfiles("test") // if you want to isolate config
 class AuthMsApplicationTests {
 
 	@BeforeAll
@@ -18,6 +24,8 @@ class AuthMsApplicationTests {
 		System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
 		System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
 		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		System.setProperty("JWT_EXPIRATION_TIME_IN_MINUTES", dotenv.get("JWT_EXPIRATION_TIME_IN_MINUTES"));
+		System.setProperty("CORS_ALLOWED_ORIGINS", dotenv.get("CORS_ALLOWED_ORIGINS"));
 	}
 
 
